8fe1c0b663107e3b8fb5fbcc42afb5d53856f05c
elantsev1 <elantsev.pa@gmail.com>
2023-04-19 12:39:08 +0300

    init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/ui/Button';
+import { FC } from 'react';
+
+interface PageProps {}
+
+const Page: FC<PageProps> = ({}) => {
+  return <Button>hello</Button>;
+};
+
+export default Page;
